c8f5f0933d2e96aeb82f8369a65585183cdce66d
Ileolami <99335756+Ileolami@users.noreply.github.com>
2024-08-09 09:29:24 +0100

    chore: Add current price to coin details in DisplayCoin component

--- src/Display/DisplayCoin.jsx
+++ src/Display/DisplayCoin.jsx
@@ -14,7 +14,7 @@ const Display = () => {
       try {
         setLoading(true);
         const apiResponse = await axios.get(`https://api.coingecko.com/api/v3/coins/${params.id}`);
-        //console.log(apiResponse);
+        console.log(apiResponse);
         setApiResponse(apiResponse)
       } catch (error) {
         console.error(error);
@@ -40,6 +40,7 @@ const Display = () => {
           <h2 className="font-bold text-lg text-gray-800 mb-2 text-center tracking-widest">DETAILs</h2>
           <div className=' leading-12 '>
             <p className="text-gray-600 mb-10 flex justify-between border-b-2">Currency <span className="font-semibold">{apiResponse.data.symbol}</span></p>
+            <p className="text-gray-600 mb-10 flex justify-between border-b-2">Current Price <span className="font-semibold">${apiResponse.data.market_data.current_price.usd}</span></p>
             <p className="text-gray-600 mb-10 flex justify-between border-b-2">Market Cap Rank <span className="font-semibold">{apiResponse.data.market_cap_rank}</span></p>
             <p className="text-gray-600 mb-10 flex justify-between border-b-2">Market Cap <span className="font-semibold">${apiResponse.data.market_data.market_cap.usd}</span></p>
             <p className="text-gray-600 mb-10 flex justify-between border-b-2">Total Supply <span className="font-medium">{apiResponse.data.market_data.total_supply}</span></p>
